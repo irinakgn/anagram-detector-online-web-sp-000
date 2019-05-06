4a14ab2bdb83658f1f74ca1fb47f4a4ddb98d41e
Irina Kagan <irinagamb@gmail.com>
2019-05-06 00:58:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,9 @@ class Anagram
     f = []
     word = @anna.chars.sort.join
     words = expression.map { |e| e.chars.sort.join  }
-words.each_with_index { |val,index| puts "index: #{index} for #{val}" if val < 30}
+    words.each_with_index { |val,index|
+
+    }
     f
   end
 
