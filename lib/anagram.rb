2d7b21ce9f438674511ebbef2e9077cda5f72cae
Irina Kagan <irinagamb@gmail.com>
2019-05-06 00:57:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,12 +12,8 @@ class Anagram
     f = []
     word = @anna.chars.sort.join
     words = expression.map { |e| e.chars.sort.join  }
-    words.each_with_index { |val,index|
-      if(val == word)
-        f << val
-      end
-    }
+    
   end
-f
+
 
 end
